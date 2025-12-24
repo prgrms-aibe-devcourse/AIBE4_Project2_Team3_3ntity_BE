4a3e95693e35bb33374bb818f0bdafd665e96404
gunheecha <gunhee2326@naver.com>
2025-12-24 17:07:21 +0900

    initial commit

---
+++ src/main/java/kr/java/java/JavaApplication.java
@@ -0,0 +1,13 @@
+package kr.java.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApplication.class, args);
+    }
+
+}
