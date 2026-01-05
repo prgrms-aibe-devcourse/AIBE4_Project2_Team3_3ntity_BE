1fa9d0d160bad87b1f0bdc38c78d43a4f0c76494
gunheecha <gunhee2326@naver.com>
2026-01-05 11:22:56 +0900

    feat: create space
    공간 도메인에 저장된 데모파일 삭제
    application.yml 파일 수정
    공간 생성 요청 DTO생성
    공간 중복 검증 메서드 레포지토리에 추가
    공간 등록 서비스 코드 구현
    공간 등록 엔드포인트 구현
    
    Closes: #7

---
+++ src/main/java/kr/java/java/domain/space/dto/SpaceRequest.java
@@ -0,0 +1,31 @@
+package kr.java.java.domain.space.dto;
+
+import kr.java.java.domain.space.entity.Space;
+import kr.java.java.domain.user.entity.User;
+
+import java.math.BigDecimal;
+
+public record SpaceRequest(
+        String title,
+        String description,
+        String category,
+        String address,
+        String detailAddress,
+        Double latitude,
+        Double longitude,
+        Integer pricePerMonth
+) {
+    public Space toEntity(User user) {
+        return Space.builder()
+                .title(this.title())
+                .description(this.description())
+                .category(this.category())
+                .address(this.address())
+                .detailAddress(this.detailAddress())
+                .latitude(BigDecimal.valueOf(this.latitude()))
+                .longitude(BigDecimal.valueOf(this.longitude()))
+                .pricePerMonth(this.pricePerMonth())
+                .user(user)
+                .build();
+    }
+}
