9a6dc0026ba669da34c228656033ec904f47fa83
Glory0206 <dmb07223@gmail.com>
2026-01-08 10:41:02 +0900

    feat; 매칭 생성 dto 분리

--- src/main/java/kr/java/java/domain/matching/dto/CreateMatchingToSpaceRequest.java
+++ src/main/java/kr/java/java/domain/matching/dto/CreateMatchingToSpaceRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
-public record CreateMatchingRequest(
+public record CreateMatchingToSpaceRequest(
         String message,
 
         @NotNull(message = "시작 날짜를 선택해주세요.")
