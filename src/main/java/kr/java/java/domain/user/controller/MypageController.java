828d7ba50771f581b7379dba9ca9877cfd2a9e2e
jivvoor <wldn040839@gmail.com>
2026-01-08 14:25:45 +0900

    refactor: import문 정리

--- src/main/java/kr/java/java/domain/user/controller/MypageController.java
+++ src/main/java/kr/java/java/domain/user/controller/MypageController.java
@@ -1,25 +1,14 @@
 package kr.java.java.domain.user.controller;
 
-import jakarta.validation.Valid;
+
 import kr.java.java.domain.auth.security.CustomUserDetails;
-import kr.java.java.domain.matching.dto.MatchingResponse;
-import kr.java.java.domain.matching.enums.MatchStatus;
-import kr.java.java.domain.portfolio.dto.PortfolioResponse;
-import kr.java.java.domain.review.dto.ReviewResponse;
-import kr.java.java.domain.space.dto.SpaceListResponse;
 import kr.java.java.domain.user.dto.MypageResponse;
-import kr.java.java.domain.user.dto.ProfileUpdateRequest;
 import kr.java.java.domain.user.service.MypageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/piece/mypages")
--- src/main/java/kr/java/java/domain/user/service/MypageService.java
+++ src/main/java/kr/java/java/domain/user/service/MypageService.java
@@ -1,34 +1,18 @@
 package kr.java.java.domain.user.service;
 
-import kr.java.java.domain.matching.dto.MatchingResponse;
-import kr.java.java.domain.matching.entity.Matching;
-import kr.java.java.domain.matching.enums.MatchStatus;
-import kr.java.java.domain.matching.repository.MatchingRepository;
-import kr.java.java.domain.matching.service.MatchingService;
-import kr.java.java.domain.portfolio.dto.PortfolioResponse;
-import kr.java.java.domain.portfolio.entity.Portfolio;
+
 import kr.java.java.domain.portfolio.repository.PortfolioRepository;
-import kr.java.java.domain.portfolio.service.PortfolioService;
-import kr.java.java.domain.review.dto.ReviewResponse;
-import kr.java.java.domain.review.entity.Review;
 import kr.java.java.domain.review.repository.ReviewRepository;
-import kr.java.java.domain.review.service.ReviewService;
-import kr.java.java.domain.space.dto.SpaceListResponse;
 import kr.java.java.domain.space.repository.SpaceRepository;
-import kr.java.java.domain.space.service.SpaceService;
 import kr.java.java.domain.user.dto.MypageResponse;
-import kr.java.java.domain.user.dto.ProfileUpdateRequest;
 import kr.java.java.domain.user.entity.User;
 import kr.java.java.domain.user.exception.UserErrorCode;
 import kr.java.java.domain.user.exception.UserException;
 import kr.java.java.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
