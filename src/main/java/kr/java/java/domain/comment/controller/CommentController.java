b8f803afbabddd9c6c2f69f8fa4c8fa5a21af482
epqmzhtm0909 <epqmzhtm0909@gmail.com>
2026-01-08 12:37:33 +0900

    feat: update comment 문의 수정 구현

--- src/main/java/kr/java/java/domain/comment/controller/CommentController.java
+++ src/main/java/kr/java/java/domain/comment/controller/CommentController.java
@@ -3,6 +3,7 @@ package kr.java.java.domain.comment.controller;
 import jakarta.validation.Valid;
 import kr.java.java.domain.comment.dto.CommentCreateRequest;
 import kr.java.java.domain.comment.dto.CommentResponse;
+import kr.java.java.domain.comment.dto.CommentUpdateRequest;
 import kr.java.java.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -97,5 +98,25 @@ public class CommentController {
         log.info("문의 삭제 완료 - 삭제된 commentId: {}", commentId);
 
         return ResponseEntity.ok("문의가 성공적으로 삭제되었습니다.");
+    }
+
+    // 문의 수정 API
+    @PatchMapping("/{commentId}")
+    public ResponseEntity<String> updateComment(
+            @PathVariable Long commentId,
+            @RequestBody CommentUpdateRequest request,
+            Long loginUserId
+    ) {
+
+        Long userId = 1L;
+        // Long userId = loginUserId; // TODO: 추후에 loginUserId로 변경
+
+        log.info("PATCH /piece/comments/{} 요청 발생 - 요청자 ID: {}", commentId, userId);
+
+        commentService.updateComment(userId, commentId, request);
+
+        log.info("문의 수정 완료 - 수정된 commentId: {}", commentId);
+
+        return ResponseEntity.ok("문의가 성공적으로 수정되었습니다.");
     }
 }
