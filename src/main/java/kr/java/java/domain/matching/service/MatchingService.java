859af6b08b56995f76a82665735df1764a1a51ee
Glory0206 <dmb07223@gmail.com>
2026-01-09 16:56:13 +0900

    feat: 대기중 매칭 원하는 시작 날짜를 지나면 자동 거절

--- src/main/java/kr/java/java/domain/matching/service/MatchingService.java
+++ src/main/java/kr/java/java/domain/matching/service/MatchingService.java
@@ -256,6 +256,30 @@ public class MatchingService {
                 log.error("[매칭 상태 변경] 실패: ID={}, 사유={}", matching.getId(), e.getMessage());
             }
         }
-        log.info("만료 매칭 처리 완료");
+    }
+
+    @Transactional
+    public void processOverdueWaitingMatchings() {
+        LocalDate today = LocalDate.now();
+
+        List<Matching> overdueMatchings = matchingRepository.findOverdueWaitingMatchings(today, MatchStatus.WAITING);
+
+        log.info("총 {}건의 만료 대상 매칭 발견", overdueMatchings.size());
+
+        for(Matching matching : overdueMatchings){
+            try{
+                matching.rejectMatch();
+
+                log.info("[매칭 거절] ID: {}, 발신자: {}, 수신자: {}",
+                        matching.getId(),
+                        matching.getUser().getNickname(),
+                        matching.getReceiver().getNickname());
+
+                // TODO: 발신자에게 알림 전송
+
+            } catch(Exception e){
+                log.error("[자동 거절 실패] ID: {}, 사유: {}", matching.getId(), e.getMessage());
+            }
+        }
     }
 }
