c60d3fc77959de653e508eb21b8e9a8478932499
Glory0206 <dmb07223@gmail.com>
2026-01-09 20:50:27 +0900

    feat: space의 썸네일 이미지 조회 쿼리

--- src/main/java/kr/java/java/domain/image/repository/ImageRepository.java
+++ src/main/java/kr/java/java/domain/image/repository/ImageRepository.java
@@ -2,6 +2,16 @@ package kr.java.java.domain.image.repository;
 
 import kr.java.java.domain.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    @Query("SELECT i.fileUrl FROM Image i WHERE i.space.id = :spaceId AND i.sortOrder = 1")
+    Optional<String> findThumnailBySpaceId(@Param("spaceId") Long spaceId);
+
+    @Query("SELECT i FROM Image i WHERE i.space.id IN :spaceIds AND i.sortOrder = 1")
+    List<Image> findThumbnailsBySpaceIds(@Param("spaceIds") List<Long> spaceIds);
 }
