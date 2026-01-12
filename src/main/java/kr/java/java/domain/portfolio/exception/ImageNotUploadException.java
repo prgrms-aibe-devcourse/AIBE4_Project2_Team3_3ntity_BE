1763b79588944209f1d088a34f1b27d9f68f06d0
gunheecha <gunhee2326@naver.com>
2026-01-12 12:44:03 +0900

    feat: link portfolio image
    커스텀 예외 추가
    이미지 업로드 로직 추가
    컨트롤러 전달자 수정
    
    Closes: #125

---
+++ src/main/java/kr/java/java/domain/portfolio/exception/ImageNotUploadException.java
@@ -0,0 +1,7 @@
+package kr.java.java.domain.portfolio.exception;
+
+public class ImageNotUploadException extends RuntimeException {
+    public ImageNotUploadException(String message) {
+        super(message);
+    }
+}
--- src/main/java/kr/java/java/domain/portfolio/service/PortfolioService.java
+++ src/main/java/kr/java/java/domain/portfolio/service/PortfolioService.java
@@ -1,10 +1,14 @@
 package kr.java.java.domain.portfolio.service;
 
 
+import kr.java.java.domain.image.enums.TargetType;
+import kr.java.java.domain.image.service.ImageService;
 import kr.java.java.domain.portfolio.dto.*;
 import kr.java.java.domain.portfolio.entity.Portfolio;
 import kr.java.java.domain.portfolio.exception.DuplicatePortfolioException;
+import kr.java.java.domain.portfolio.exception.ImageNotUploadException;
 import kr.java.java.domain.portfolio.exception.NotFoundPortfolioException;
+import kr.java.java.domain.portfolio.exception.PortfolioDeleteException;
 import kr.java.java.domain.portfolio.repository.PortfolioRepository;
 import kr.java.java.domain.space.exception.NotFoundUserException;
 import kr.java.java.domain.space.exception.UnAuthorizedException;
@@ -14,7 +18,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -23,9 +29,10 @@ import java.util.List;
 public class PortfolioService {
     private final PortfolioRepository portfolioRepository;
     private final UserRepository userRepository;
+    private final ImageService imageService;
 
     @Transactional
-    public void createPortfolio(PortfolioRequest portfolioRequest, Long loginUserId){
+    public void createPortfolio(PortfolioRequest portfolioRequest, Long loginUserId, List<MultipartFile> images){
         if (portfolioRepository.existsByBrandNameAndTitle(portfolioRequest.brandName(), portfolioRequest.title())) {
             log.error("동일한 포트폴리오가 존재합니다.");
             throw new DuplicatePortfolioException("동일한 포트폴리오가 존재합니다.");
@@ -33,8 +40,19 @@ public class PortfolioService {
 
         // TODO 로그인한 유저 권한검증 추가예정
         User user = userRepository.getReferenceById(loginUserId);
+
         Portfolio portfolio = portfolioRequest.toEntity(user);
         portfolioRepository.save(portfolio);
+
+        if (images != null && !images.isEmpty()) {
+            try {
+                // TargetType.SPACE와 방금 만든 space.getId()를 넘김
+                imageService.uploadImage(images, TargetType.SPACE, portfolio.getId());
+            } catch (IOException e) {
+                log.error("이미지 업로드 실패", e);
+                throw new ImageNotUploadException("이미지 업로드 중 오류가 발생했습니다.");
+            }
+        }
     }
 
     @Transactional(readOnly = true)
@@ -74,7 +92,7 @@ public class PortfolioService {
             portfolioRepository.delete(portfolio);
         } catch (Exception e) {
             log.error("포트폴리오 삭제 실패 (참조 데이터 존재)");
-            throw new RuntimeException("현재 예약 내역이 있어 삭제할 수 없습니다.");
+            throw new PortfolioDeleteException("현재 예약 내역이 있어 삭제할 수 없습니다.");
         }
     }
 
