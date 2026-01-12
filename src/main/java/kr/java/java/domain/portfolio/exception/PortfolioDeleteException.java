de3519e0a9a8dbe19dc83dbda462ae55758642e0
gunheecha <gunhee2326@naver.com>
2026-01-12 12:44:03 +0900

    feat: link portfolio image
    커스텀 예외 추가
    이미지 업로드 로직 추가
    컨트롤러 전달자 수정
    
    Closes: #125

---
+++ src/main/java/kr/java/java/domain/portfolio/exception/PortfolioDeleteException.java
@@ -0,0 +1,7 @@
+package kr.java.java.domain.portfolio.exception;
+
+public class PortfolioDeleteException extends RuntimeException {
+    public PortfolioDeleteException(String message) {
+        super(message);
+    }
+}
