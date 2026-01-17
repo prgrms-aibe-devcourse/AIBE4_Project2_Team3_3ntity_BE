60a9660677f9a42888a1c1860f73e07fb548c597
Glory0206 <dmb07223@gmail.com>
2026-01-17 17:44:59 +0900

    refactor: Space, Matching의 NotFountUserException 삭제

--- src/main/java/kr/java/java/domain/matching/exception/NotFoundUserException.java
+++
@@ -1,7 +0,0 @@
-package kr.java.java.domain.matching.exception;
-
-public class NotFoundUserException extends RuntimeException {
-    public NotFoundUserException(String message) {
-        super(message);
-    }
-}
--- src/main/java/kr/java/java/domain/matching/service/MatchingService.java
+++ src/main/java/kr/java/java/domain/matching/service/MatchingService.java
@@ -13,11 +13,12 @@ import kr.java.java.domain.portfolio.repository.PortfolioRepository;
 import kr.java.java.domain.review.repository.ReviewRepository;
 import kr.java.java.domain.space.entity.Space;
 import kr.java.java.domain.space.exception.NotFoundSpaceException;
-import kr.java.java.domain.space.exception.NotFoundUserException;
 import kr.java.java.domain.space.repository.SpaceRepository;
 import kr.java.java.domain.space.service.SpaceService;
 import kr.java.java.domain.user.dto.UserFormResponse;
 import kr.java.java.domain.user.entity.User;
+import kr.java.java.domain.user.exception.UserErrorCode;
+import kr.java.java.domain.user.exception.UserException;
 import kr.java.java.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -41,8 +42,6 @@ public class MatchingService {
     private final SpaceImageService  spaceImageService;
     private final SpaceService spaceService;
     private final ReviewRepository reviewRepository;
-
-    //TODO 해당 서비스 페이지에 있는 User 에러처리는 추후 User 도메인의 exception에 생기면 변경
 
     @Transactional
     public void createUserToSpace(
@@ -72,9 +71,9 @@ public class MatchingService {
             UUID userUuid
     ) {
         User sender = userRepository.findByUuid(userUuid)
-                .orElseThrow(() -> new NotFoundUserException("로그인 유저 없음"));
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
         User receiver = userRepository.findById(targetUserId)
-                .orElseThrow(() -> new NotFoundUserException("상대방 유저를 찾을 수 없습니다."));
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
         UUID targetUserUuid = receiver.getUuid();
         Space space = spaceRepository.findById(request.spaceId())
                 .orElseThrow(() -> new NotFoundSpaceException("해당 공간이 없습니다."));
@@ -100,13 +99,13 @@ public class MatchingService {
     ) {
         log.info("[매칭 service] 매칭 생성 시작");
         User sender = userRepository.findByUuid(command.senderUuid())
-                .orElseThrow(() -> new NotFoundUserException("로그인 유저 없음"));
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
         log.info("[매칭 service] 발신자 확인: ID={}, Nickname={}, UUID={}",
                 sender.getId(), sender.getNickname(), sender.getUuid());
 
         // 2. 수신자(Receiver) 조회 및 로그
         User receiver = userRepository.findByUuid(command.receiverUuid())
-                .orElseThrow(() -> new NotFoundUserException("수신 유저 없음"));
+                .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
         log.info("[매칭 service] 수신자 확인: ID={}, Nickname={}, UUID={}",
                 receiver.getId(), receiver.getNickname(), receiver.getUuid());
 
